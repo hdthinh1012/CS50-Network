5cc6aea331f6b591e056cfaf9f11f22839a94265
hdthinhpro1012 <hdthinh01@gmail.com>
2021-05-17 11:34:28 +0700

    files updates

--- network/signals.py
+++ network/signals.py
@@ -7,5 +7,5 @@ from .views import *
 @receiver(post_save, sender=Message)
 def updateMessageUI(sender, instance, created, **kwargs):
     if created:
-        print("Message send")
+        print("Signals trigger database command only, cannot trigger asynchronous function need request method, using Django Channel instead")
  
